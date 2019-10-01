f62a7948c4a8606d0aaa7a9974d7610fa24b44d2
Steven Moreland <smoreland@google.com>
2019-10-01 11:01:16 -0700

    land: Remove libhwbinder/libhidltransport deps
    Since these were combined into libhidlbase.
    
    Bug: 135686713
    Test: build only (libhwbinder/libhidltransport are empty)
    Change-Id: I640a3f0e0839214523627bf0b59fd5ae9dd2c61c

--- libhidl/Android.mk
+++ libhidl/Android.mk
@@ -14,21 +14,27 @@
 # limitations under the License.
 
 include $(CLEAR_VARS)
-LOCAL_SHARED_LIBRARIES := libhidltransport
+
+LOCAL_SHARED_LIBRARIES := \
+    libhidlbase
+
 LOCAL_MODULE := android.hidl.base@1.0
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_SHARED_LIBRARIES := libhidltransport
+
+LOCAL_SHARED_LIBRARIES := \
+    libhidlbase
+
 LOCAL_MODULE := android.hidl.manager@1.0
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_SHARED_LIBRARIES := libhidltransport
+LOCAL_SHARED_LIBRARIES := libhidlbase
 LOCAL_MODULE := android.hidl.base@1.0_system
 LOCAL_INSTALLED_MODULE_STEM := android.hidl.base@1.0.so
 LOCAL_MODULE_TAGS := optional
@@ -36,7 +42,7 @@ LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_SHARED_LIBRARIES := libhidltransport
+LOCAL_SHARED_LIBRARIES := libhidlbase
 LOCAL_MODULE := android.hidl.manager@1.0_system
 LOCAL_INSTALLED_MODULE_STEM := android.hidl.manager@1.0.so
 LOCAL_MODULE_TAGS := optional
