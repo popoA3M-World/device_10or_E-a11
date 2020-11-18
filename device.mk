d8ddd1fad393542873ca36da609892c89889faf8
Volodymyr Zhdanov <wight554@gmail.com>
2020-11-18 21:45:41 +0100

    land: build vendor version of libavservices_minijail
    Change-Id: Ic5cde5de9dc5f003ef8436cc44606c32faa5beb5

--- device.mk
+++ device.mk
@@ -345,6 +345,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Minijail
+PRODUCT_PACKAGES += \
+    libavservices_minijail.vendor
+
 # Offline charger
 PRODUCT_PACKAGES += \
     charger_res_images \
