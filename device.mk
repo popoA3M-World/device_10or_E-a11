61a608775bfb199ca3d9501a77d1b3845d8e2dfb
sairam1411 <majgaonkars3@gmail.com>
2021-01-16 21:15:47 +0530

    fix derp
    Signed-off-by: sairam1411 <majgaonkars3@gmail.com>

--- device.mk
+++ device.mk
@@ -111,7 +111,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
-    android.hardware.camera.provider@2.6
+    android.hardware.camera.provider@2.6 \
+    vendor.qti.hardware.camera.device@1.0
 
 # Bluetooth
 PRODUCT_PACKAGES += \
