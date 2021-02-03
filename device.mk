ddc39ebfc37a67645c5efd13e3bb1128886180e9
Amit Pundir <amit.pundir@linaro.org>
2021-02-04 00:59:47 +0400

    land: mediaswcodec: Fix selinux and seccomp policy denials
    Fix mediaswcodec selinux and seccomp denials for video
    playback to work with software codecs.
    
    mediaswcodec need gpu access, plus it also crashes with
    following seccomp error during video playback:
    
    E media.swcodec: libminijail[2139]: blocked syscall: sysinfo
    
    So whitelist sysinfo syscall for mediaswcodec.
    
    Change-Id: I11db36aeda475c4ca73121efb8b2bfd3d7590be0
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>
    Signed-off-by: AlexThundrous <alexthundrous2104@gmail.com>
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -419,7 +419,8 @@ PRODUCT_PACKAGES += \
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/vendor.qti.hardware.dsp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/vendor.qti.hardware.dsp.policy
+    $(LOCAL_PATH)/seccomp/vendor.qti.hardware.dsp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/vendor.qti.hardware.dsp.policy \
+    $(LOCAL_PATH)/seccomp/mediaswcodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaswcodec.policy \
 
 # Sensors
 PRODUCT_PACKAGES += \
