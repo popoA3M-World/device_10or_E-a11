d3084e5229048bc700539543603edcfdc28594df
sairam1411 <majgaonkars3@gmail.com>
2021-02-06 19:36:10 +0530

    land: Build libjson
    For fixing:-
    F/linker  (  902): CANNOT LINK EXECUTABLE "xtra-daemon": library "libjson.so" not found: needed by /vendor/lib64/libpdmapper.so in namespace (default)
    
    Signed-off-by: sairam1411 <majgaonkars3@gmail.com>

--- device.mk
+++ device.mk
@@ -369,6 +369,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
+# QMI
+PRODUCT_PACKAGES += \
+    libjson
+
 # RIL
 PRODUCT_PACKAGES += \
     librmnetctl \
