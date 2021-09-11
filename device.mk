5bd01d5a9886430ba758a57073722273bfbed298
sairam1411 <majgaonkars3@gmail.com>
2021-09-11 23:27:30 +0530

    land: Add prebuilt libunwind.so
    Signed-off-by: sairam1411 <majgaonkars3@gmail.com>

--- device.mk
+++ device.mk
@@ -180,8 +180,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.land \
-    libunwind
+    android.hardware.biometrics.fingerprint@2.1-service.land
 
 # FM
 PRODUCT_PACKAGES += \
