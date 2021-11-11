5bf3fdbe8504771d32a24355aa95ddf1aba3bef6
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-11-11 13:00:34 +0530

    E: Update copyright headers
    * Move to SPDX license identifier
    
    * Update copyrights to 2021

--- extract-files.sh
+++ extract-files.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2021 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
+#
 
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
     return
