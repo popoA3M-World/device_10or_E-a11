08dc2af7efee549ce52ffb603f97376eb81afbe1
popoASM <pratyayaborborah@gmail.com>
2021-11-11 14:26:22 +0530

    E: Adapt light hal for Tenor E
    Signed-off-by: popoASM <pratyayaborborah@gmail.com>

--- device.mk
+++ device.mk
@@ -260,7 +260,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_land
+    android.hardware.light@2.0-service.E
 
 # Libshim
 PRODUCT_PACKAGES += \
