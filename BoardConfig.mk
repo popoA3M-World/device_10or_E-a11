4f62ec4d55c09355997f5f17881a4a84c576b864
ZeeLog <Danjel.Zammit@gmail.com>
2021-11-12 12:01:23 +0530

    E: Import init from zeelog's mido tree
    Signed-off-by: popoASM <pratyayaborborah@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -150,6 +150,10 @@ DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_E
+TARGET_RECOVERY_DEVICE_MODULES := libinit_E
+
 # Libshim
 TARGET_LD_SHIM_LIBS := /vendor/bin/mm-qcamera-daemon|vendor/lib/libshims_camera.so
 
